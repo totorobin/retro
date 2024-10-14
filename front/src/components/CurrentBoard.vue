d8f8e55088bef5433024605da137e0461cbbea4e
totorobin <totorobin@gmail.com>
2024-10-14 22:49:40 +0200

    css

--- front/src/components/CurrentBoard.vue
+++ front/src/components/CurrentBoard.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="board">
-    {{ board }}
+    <UsersMenu class="panel-right-top" :users="boardStore.board?.users"></UsersMenu>
+    vous êtes connecter au board {{ board.name }}
   </div>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
 import {useBoardStore} from "../stores/board.ts";
+import UsersMenu from "./UsersMenu.vue";
 
 const boardStore = useBoardStore();
 
